fc783e415c695a56ed3499eaa4eb3b11b577c77d
Nikita <nvadeveloper@gmail.com>
2022-04-14 22:32:55 +0300

    add btn to slider

--- pages/boats/index.js
+++ pages/boats/index.js
@@ -1,3 +1,4 @@
+import CardWithCorusel from "../../components/CardWithCorusel"
 
 
 const Boats = () => {
@@ -5,6 +6,7 @@ const Boats = () => {
 return (
   <div className="container mx-auto px-4 sm:px-6 lg:px-8">
     <p className="mt-10 font-bold text-3xl">Катера</p>
+    <CardWithCorusel />
   </div>
 )}
 
