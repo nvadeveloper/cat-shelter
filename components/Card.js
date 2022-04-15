b07cd6c13afed84106dda16dc859f8619a6aa32e
Nikita <nvadeveloper@gmail.com>
2022-04-15 15:57:22 +0300

    add CardСarousel and Card

--- components/Card.js
+++ components/Card.js
@@ -1,32 +1,22 @@
 import CardСarousel from "./CardСarousel"
 
-
-const Card = ({images, name, tag, price, people}) => {
+const Card = ({ images, name, tag, price, people }) => {
 
     return (
         <>
             <CardСarousel images={images} />
             <div className="flex justify-between mt-2">
-            <div>
-                <p className="font-medium text-xl">{name}</p>
-                <p className="text-sm text-gray-500">{tag}</p>
+                <div>
+                    <p className="font-medium text-xl">{name}</p>
+                    <p className="text-sm text-gray-500">{tag}</p>
+                </div>
+                <div className="flex flex-col items-end">
+                    <p className="text-sm text-gray-900 mt-2">{price}</p>
+                    <p className="text-sm text-gray-900">{people}</p>
+                </div>
             </div>
-            <div className="flex flex-col items-end">
-                <p className="text-sm text-gray-900 mt-2">{price}</p>
-                <p className="text-sm text-gray-900">{people}</p>
-            </div>
-            </div>
-
-            {/* <div>
-                <p className="font-medium text-xl">Chaparral</p>
-                <p className="text-sm text-gray-500">Бизнес класс</p>
-            </div>
-            <div className="flex flex-col items-end">
-                <p className="text-sm text-gray-900 mt-2">10 000 руб/час</p>
-                <p className="text-sm text-gray-900">8 человек</p>
-            </div>
-            </div> */}
         </>
-    )}
-    
+    )
+}
+
 export default Card
--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,11 @@
 
 const About = () => {
 
-    return (
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <p className="mt-10 font-bold text-3xl">О нас</p>
-      </div>
-    )}
-    
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <p className="mt-10 font-bold text-3xl">О нас</p>
+    </div>
+  )
+}
+
 export default About
