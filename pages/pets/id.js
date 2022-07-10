51a9fce35c1bcf03e770a6c0922d72d46f02a552
Nikita <nvadeveloper@gmail.com>
2022-07-11 02:06:37 +0300

    add pets - id page

---
+++ pages/pets/id.js
@@ -0,0 +1,9 @@
+const Pet = () => {
+    return (
+        <section className="container mx-auto px-4 sm:px-6 lg:px-8  mb-8">
+            <h1 className="font-bold text-2xl mb-8">Чивас</h1>
+        </section>
+    );
+};
+
+export default Pet;
--- pages/pets/index.js
+++ pages/pets/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Pets = () => {
     return (
@@ -7,15 +8,20 @@ const Pets = () => {
 
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
                 <div>
-                    <div className="w-full h-64 sm:h-96 relative object-cover	">
-                        <Image
-                            src="https://sun1-20.userapi.com/impg/rHQ9DpMVXODYNuI67UAPYLlOKKqSGKTZ7JLUIA/y8HrvE59Bgw.jpg?size=1040x0&quality=95&sign=19028bf2c30f644cda3aa154d49d101a"
-                            className="rounded-lg"
-                            layout="fill"
-                            objectFit="cover"
-                            alt="image"
-                        />
-                    </div>
+                    <Link href="/pets/id">
+                        <a>
+                            <div className="w-full h-64 sm:h-96 relative object-cover bg-gray-100">
+                                <Image
+                                    src="https://sun1-20.userapi.com/impg/rHQ9DpMVXODYNuI67UAPYLlOKKqSGKTZ7JLUIA/y8HrvE59Bgw.jpg?size=1040x0&quality=95&sign=19028bf2c30f644cda3aa154d49d101a"
+                                    className="rounded-lg"
+                                    layout="fill"
+                                    objectFit="cover"
+                                    alt="image"
+                                />
+                            </div>
+                        </a>
+                    </Link>
+
                     <div className="mt-2 flex items-center justify-between">
                         <p className="text-2xl font-bold">Чивас</p>
                         <div className="flex space-x-2">
@@ -26,14 +32,18 @@ const Pets = () => {
                 </div>
 
                 <div>
-                    <div className="w-full h-64 sm:h-96 relative object-cover	">
-                        <Image
-                            src="https://sun1-20.userapi.com/impg/rHQ9DpMVXODYNuI67UAPYLlOKKqSGKTZ7JLUIA/y8HrvE59Bgw.jpg?size=1040x0&quality=95&sign=19028bf2c30f644cda3aa154d49d101a"
-                            className="rounded-lg"
-                            layout="fill"
-                            objectFit="cover"
-                        />
-                    </div>
+                    <Link href="/pets/id">
+                        <a>
+                            <div className="w-full h-64 sm:h-96 relative object-cover	">
+                                <Image
+                                    src="https://sun1-20.userapi.com/impg/rHQ9DpMVXODYNuI67UAPYLlOKKqSGKTZ7JLUIA/y8HrvE59Bgw.jpg?size=1040x0&quality=95&sign=19028bf2c30f644cda3aa154d49d101a"
+                                    className="rounded-lg"
+                                    layout="fill"
+                                    objectFit="cover"
+                                />
+                            </div>
+                        </a>
+                    </Link>
                     <div className="mt-2 flex items-center justify-between">
                         <p className="text-2xl font-bold">Чивас</p>
                         <div className="flex space-x-2">
